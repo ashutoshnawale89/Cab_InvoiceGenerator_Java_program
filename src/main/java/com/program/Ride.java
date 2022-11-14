b1185465f6ed22a5f5fa895c829e12663276e07d
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-14 08:29:51 +0530

    Refactor : Create Invoice For Multiple Rides And Test it

---
+++ src/main/java/com/program/Ride.java
@@ -0,0 +1,12 @@
+package com.program;
+
+public class Ride {
+	public final int time;
+    public double distance;
+    public Ride(double distance, int time){
+        this.distance = distance;
+        this.time = time;
+    }
+	
+
+}
--- src/test/java/InvoiceGenerator_J_UnitTest.java
+++ src/test/java/InvoiceGenerator_J_UnitTest.java
@@ -2,13 +2,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.program.InvoiceGenerator;
+import com.program.Ride;
 public class InvoiceGenerator_J_UnitTest {
 	@Test
-	public void calculateFareOfTheOneRides()  {
+	public void CalculateFar()  {
 		InvoiceGenerator obj=new InvoiceGenerator();
 		double test=obj.calculateFare(10, 15);
-		Assert.assertEquals(115.0, test, 0.0);
+		double test1=obj.calculateFare(10, 15)+obj.calculateFare(10, 15);;
+		Assert.assertEquals(230.0, test1, 0.0);
 	}
-
-
-}
+	@Test
+	public void MonthFare()  {
+		InvoiceGenerator obj=new InvoiceGenerator();
+		Ride[] rides= { new Ride(1,23), new Ride(1,2)};
+		double monthfare=obj.calculateFare(rides);
+		Assert.assertEquals(45.0,monthfare , 0.0);
+		
+		}
+	}
