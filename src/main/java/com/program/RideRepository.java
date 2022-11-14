dd0895845773379f2d8e3100fde77b9ce290d4ae
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-11-14 10:21:32 +0530

    Refactor: Invoice Service Program

--- src/main/java/com/program/RideRepository.java
+++ src/main/java/com/program/RideRepository.java
@@ -8,15 +8,16 @@ import java.util.Map;
 public class RideRepository {
 	Map<String, ArrayList<Ride>> userRides;
 
-	public RideRepository(){
-		this.userRides = new HashMap<>();
-	}
-	public void addRide(String userId, Ride[] rides) {
-		this.userRides.put(userId, new ArrayList<>(Arrays.asList(rides)));
-	}
+    public RideRepository(){
+        this.userRides = new HashMap<>();
+    }
 
-	public Ride[] getRides(String userId) {
-		return this.userRides.get(userId).toArray(new Ride[0]);
-	}
+    public void addRide(String userId, Ride[] rides) {
+        this.userRides.put(userId, new ArrayList<>(Arrays.asList(rides)));
+    }
+
+    public Ride[] getRides(String userId) {
+        return this.userRides.get(userId).toArray(new Ride[0]);
+    }
 
 }
